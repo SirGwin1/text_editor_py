ddd009ede8c57f8057076e69bfdc6e303b0bc634
SirGwin1 <akorgodwin01@gmail.com>
2026-09-08 11:10:28 +0100

    building the framework for a function that removes duplicates from text

--- functions/fix_grammar.py
+++ functions/fix_grammar.py
@@ -1,9 +1,20 @@
 def fix_grammar(text):
 
     ## unnecessary space removal
-    words = text.split
+    words = text.split()
 
     for word in words:
         if word 
 
     return " ".Join(words)
+
+
+def remove_duplicate_words(text):
+    words = text.split()
+    unique_words = []
+
+    for word in words:
+        if word not in unique_words:
+            unique_words.append(word)
+
+    return " ".join(unique_words)
